0d41a704752d335eb6c7284899e81cd52f1c49c5
Anselmo Del Hoyo <anselmodelhoyo11@gmail.com>
2023-11-30 19:25:56 +0100

    Simple data in Python

---
+++ Data-types/simple_data.py
@@ -0,0 +1,27 @@
+
+# STRING (Cadenas de texto)
+
+"string"
+'string'
+
+"""
+    Este es un texto con
+    más de una línea.
+"""
+'''
+    Este es un texto con
+    más de una línea.
+'''
+
+# INT (Números enteros)
+
+11
+
+# FLOAT (Números con coma)
+
+13.2
+
+# BOOL (Valores booleanos: True o False)
+
+False
+True
