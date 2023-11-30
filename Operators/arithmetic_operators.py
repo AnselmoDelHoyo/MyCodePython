e5dd087c833545c53c4b278510de742defa7814e
Anselmo Del Hoyo <anselmodelhoyo11@gmail.com>
2023-11-30 23:32:45 +0100

    Arithmetic operators in Python

---
+++ Operators/arithmetic_operators.py
@@ -0,0 +1,37 @@
+
+# Operadores aritméticos
+
+# suma y resta (+ y -)
+
+suma = 12 + 5
+resta = 12 - 5
+
+# Multiplicación y división (* y /)
+
+multiplicacion = 4 * 3
+division = 23 / 7 # Devuelve un dato float (flotante)
+
+# División baja -> Devuelve el entero de la división
+
+division_baja = 23 // 7
+
+# Potenciación
+
+potenciacion = 2 ** 3
+
+# Resto o módulo -> Devuelve el resto de la división 
+
+resto = 10 % 5
+
+# type(dato) recibe un dato y devuelve su tipo 
+
+data_type = type(division_baja)
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(division_baja)
+print(potenciacion)
+print(resto)
+print(data_type)
