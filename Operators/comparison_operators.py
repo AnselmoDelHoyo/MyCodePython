af979557829fd5573dcdb4bc55c97bf5061f85ef
Anselmo Del Hoyo <anselmodelhoyo11@gmail.com>
2023-12-15 21:49:40 +0100

    Comparison operators

---
+++ Operators/comparison_operators.py
@@ -0,0 +1,32 @@
+
+# Operadores de comparación
+
+es_igual_que = 5 == 6
+
+distinto_de = 5 != 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual = 5 >= 6
+
+menor_o_igual = 5 <= 6
+
+print(mayor_o_igual)
+
+# Calculos combinados
+
+a = 5
+b = 10
+c = 20
+comparacion = a + b < c
+
+print(comparacion)
+
+# Comparar usuarios
+
+contraseña_almacenada = "Adhc45"
+contraseña_escrita = '''Adhc54'''
+
+print(contraseña_almacenada == contraseña_escrita)
